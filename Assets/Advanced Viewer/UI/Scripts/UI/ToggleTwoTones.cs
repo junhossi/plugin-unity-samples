7fe1be72f2cb2a3800a7ca742cd5170b197d2b4f
Olivier Giniaux <oginiaux@gmail.com>
2019-02-01 19:04:23 +0100

    Add draggable and resizable components for runtime UI rects

---
+++ Assets/Advanced Viewer/UI/Scripts/UI/ToggleTwoTones.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle))]
+public class ToggleTwoTones : MonoBehaviour
+{
+    private Toggle toggle;
+
+    void Start() {
+        toggle = GetComponent<Toggle>();
+        toggle.onValueChanged.AddListener(OnToggleValueChanged);
+
+        OnToggleValueChanged(toggle.isOn);
+    }
+
+    private void OnToggleValueChanged(bool isOn) {
+        ColorBlock cb = toggle.colors;
+        if (!isOn) {
+            cb.normalColor = Color.gray;
+            cb.highlightedColor = Color.gray;
+        } else {
+            cb.normalColor = Color.white;
+            cb.highlightedColor = Color.white;
+        }
+        toggle.colors = cb;
+    }
+}
