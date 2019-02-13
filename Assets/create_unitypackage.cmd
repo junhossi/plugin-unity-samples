1999c64a3f09e85793f44e4334be82a19c295d41
Olivier Giniaux <oginiaux@gmail.com>
2019-02-13 17:32:20 +0100

    Fix path issue in unitypackage generation

--- Assets/create_unitypackage.cmd
+++ Assets/create_unitypackage.cmd
@@ -1,2 +1,2 @@
-..\Tools\UnityPacker.exe . "Plugin-for-Unity-Samples-master" "_" "Plugins/PiXYZ/Samples/" "exe,dll,unitypackage,cmd" "Plugins"
+..\Tools\UnityPacker.exe . "Plugin-for-Unity-Samples-master" "_" "Assets/Plugins/PiXYZ/Samples/" "exe,dll,unitypackage,cmd" "Plugins"
 pause
