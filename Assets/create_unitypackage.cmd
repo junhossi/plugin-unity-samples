d9c4cfe540264caec078967c009b032e5129fc23
Olivier Giniaux <oginiaux@gmail.com>
2019-05-16 14:04:25 +0200

    Update UnityPacker to fix .meta doublons

--- Assets/create_unitypackage.cmd
+++ Assets/create_unitypackage.cmd
@@ -1,2 +1,2 @@
-..\Tools\UnityPack.exe mode=pack folder="." name="Package" root="Assets/Plugins/PiXYZ/Samples" ignore="(Plugins\\.*|unitypackage|exe|exe|cmd)$"
+..\Tools\UnityPacker.exe mode=pack folder="." name="Package" root="Assets/Plugins/PiXYZ/Samples" ignore="(Plugins\\.*|unitypackage|exe|exe|cmd)$"
 pause
