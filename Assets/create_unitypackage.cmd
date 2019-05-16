d3b4bc9df843161a304d4482157f3373c156032f
Olivier Giniaux <oginiaux@gmail.com>
2019-05-16 12:11:45 +0200

    Fix packer, Improve lighting in Advanced Viewer

--- Assets/create_unitypackage.cmd
+++ Assets/create_unitypackage.cmd
@@ -1,2 +1,2 @@
-..\Tools\UnityPacker.exe . "Plugin-for-Unity-Samples-master" "_" "Assets/Plugins/PiXYZ/Samples/" "exe,dll,unitypackage,cmd" "Plugins"
+..\Tools\UnityPack.exe mode=pack folder="." name="Package" root="Assets/Plugins/PiXYZ/Samples" ignore="(Plugins\\.*|unitypackage|exe|exe|cmd)$"
 pause
