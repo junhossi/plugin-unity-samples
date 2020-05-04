246d20daa9a3c429e97048b175c3f24823ed8378
Olivier Giniaux <olivier.giniaux@pi.xyz>
2020-05-04 10:55:26 +0200

    Update unitypacker and project version

--- create_unitypackage.cmd
+++ create_unitypackage.cmd
@@ -1,2 +1,2 @@
-Tools\UnityPacker.exe mode=pack folder="Assets" name="plugin-unity-samples" root="Assets/Plugins/PiXYZ/Samples" ignore="(Plugins\\.*|unitypackage|exe|exe|cmd)$"
+Tools\UnityPacker.exe mode pack folder "Assets" package "plugin-unity-samples" root "Assets/Plugins/PiXYZ/Samples" ignore "(Plugins\\.*|unitypackage|exe|exe|cmd)$"
 pause
